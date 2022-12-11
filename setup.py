50c2c750097afd7fec4563a42d7e4a3dc6c450c0
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-11 10:57:30 +0330

    setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='bing-background',
+    name='coinmarketcap',
     version='0.0.1',
     author='hirbod aflaki',
     author_email='hirbodprime@gmail.com',
@@ -13,9 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/hirbodprime/coinmarketcap.git',
-    # project_urls = {
-        # "Bug Tracker": "https://git.pe42.ir/hirbod/bing-background-python/issues"
-    # },
+
     keywords=['python', 'coin','coinmarketcap' 'beautifulsoup','requests','data','coin data'],
     classifiers=[
         "Development Status :: 1 - Planning",
