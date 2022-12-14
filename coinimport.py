7ac0fcaebaff156b14a6e47a59a375dbe746be34
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-12-14 09:42:40 +0330

    Changes: how to use download logo by symbol

--- coinimport.py
+++ coinimport.py
@@ -1,3 +1,5 @@
-from coinmarketcapscraper.coinsmarketcap import scraper
-app = Scraper(downloadlogo=True , coin_data=True,coin_data_file=True)
+from coinmarketcapscraper.coinsmarketcap import Scraper
+# if seting download_all_logos to True don't set download_logo_sybmol={symbol}
+app = Scraper(download_all_logos=True , coin_data=True,coin_data_file=True)
+
 app.run()
